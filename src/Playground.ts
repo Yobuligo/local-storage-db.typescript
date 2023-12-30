8b73c92032606e591e5026fff76d54cfb71cbc87
Yobuligo <hoffmannpeter82@gmail.com>
2023-12-30 14:39:38 +0100

    Add usecase example for assigning certificate to person

--- src/Playground.ts
+++ src/Playground.ts
@@ -1,5 +1,7 @@
 import { Database } from "./database/Database";
 import { IRecord } from "./record/IRecord";
+import { MemoryStorage } from "./storage/MemoryStorage";
+import { StorageFactory } from "./storage/StorageFactory";
 import { Table } from "./table/Table";
 
 interface IPerson extends IRecord<number> {
@@ -13,7 +15,30 @@ interface ICertificate extends IRecord<number> {
 
 class DCertificate extends Table<ICertificate> {}
 
-class DPerson extends Table<IPerson> {}
+class DPerson extends Table<IPerson> {
+  /**
+   * Access the certificates of a person
+   */
+  certificates = this.oneToMany(DCertificate);
+}
 
-const db = new Database("demo")
-const Person = db.define<IPerson>("persons").build({tableDefinition: DPerson})
+StorageFactory.storageType = MemoryStorage
+
+const db = new Database("demo");
+const Certificate = db
+  .define<ICertificate>("certificates")
+  .build({ tableDefinition: DCertificate });
+
+const certificate = Certificate.insert({ title: "Bachelor" });
+const certificates = Certificate.select();
+
+const Person = db
+  .define<IPerson>("persons")
+  .build({ tableDefinition: DPerson });
+
+const person = Person.insert({ firstname: "Stacey", lastname: "Starfish" });
+
+const persons = Person.select();
+
+debugger;
+// Person.certificates.insert(person, certificate);
